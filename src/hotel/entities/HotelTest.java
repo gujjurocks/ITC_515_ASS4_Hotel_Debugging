2ee0b690785c53bba44f1dab9b2cf6b9ce4e297b
gujjurocks <41317723+gujjurocks@users.noreply.github.com>
2018-10-14 12:10:37 +1100

    HotelTest file

---
+++ src/hotel/entities/HotelTest.java
@@ -0,0 +1,81 @@
+package hotel.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HotelTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testHotel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testAddRoom() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testIsRegistered() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testRegisterGuest() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFindGuestByPhoneNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFindActiveBookingByRoomId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFindAvailableRoom() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFindBookingByConfirmationNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testBook() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testCheckin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testAddServiceCharge() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testCheckout() {
+		fail("Not yet implemented"); // TODO
+	}
+
+
+
+}
